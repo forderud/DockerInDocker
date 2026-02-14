20b833080c484e26b31b4b7539e4b4d1fbe02d66
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-02-14 13:38:39 +0100

    wip

--- test.bat
+++ test.bat
@@ -1,15 +1,20 @@
 @echo off
 
 ::echo "Testing Ubuntu image build..."
-::docker build .
+::docker build -f Ubuntu-24.04.Dockerfile .
 ::pause
 
 ::To investigate:
 ::* DOCKER_TLS_CERTDIR
 
-echo "Testing Docker-in-Docker..."
+echo "Building docker-in-docker..."
+docker build -f dind.Dockerfile .
+pause
+
+
+::echo "Testing Docker-in-Docker..."
 :: /usr/local/bin/docker
-docker run --privileged --rm -it docker:dind "sh"
+::docker run --privileged --rm -it docker:dind "sh"
 
 ::TLS test:
 :: wget https://raw.githubusercontent.com/forderud/QtWasm/refs/heads/master/README.md
