93452d70685fc98faedf4211f56aa3d89ecbab5a
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-02-15 20:47:12 +0100

    .

--- test.bat
+++ test.bat
@@ -14,11 +14,13 @@ docker build -t gehc-dind -f dind.Dockerfile .
 
 echo Testing Docker-in-Docker...
 
-set MAP_DOCKER_SOCKET=-v /var/run/docker.sock:/var/run/docker.sock
-::docker run --privileged %MAP_DOCKER_SOCKET% --rm -it gehc-dind sh
+set DIND_PARAMS=-v /var/run/docker.sock:/var/run/docker.sock
+::set DIND_PARAMS=--privileged
+
+::docker run %DIND_PARAMS% --rm -it gehc-dind sh
 
 :: ERROR: failed to connect to the docker API at tcp://docker:2375: lookup docker on 192.168.65.7:53: no such host
-docker run --privileged %MAP_DOCKER_SOCKET% --rm -it gehc-dind sh -c "echo Trying to run docker..; docker build -f Ubuntu-24.04.Dockerfile ."
+docker run %DIND_PARAMS% --rm -it gehc-dind sh -c "echo Trying to run docker..; docker build -f Ubuntu-24.04.Dockerfile ."
 
 ::TLS test:
 :: wget https://raw.githubusercontent.com/forderud/QtWasm/refs/heads/master/README.md
