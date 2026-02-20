6387ab1b03ce381e78f04da8e7bfe8e8057451bd
Fredrik Orderud <forderud@gmail.com>
2026-02-20 18:27:33 +0100

    Remove DOCKER_TLS_CERTDIR parameters that wasn't needed in the first place.

--- DinD_test.bat
+++ DinD_test.bat
@@ -4,16 +4,7 @@ echo Building custom docker-in-docker image...
 docker build -t gehc-dind -f dind.Dockerfile .
 ::pause
 
-
 echo Testing Docker-in-Docker...
-
-:: Docker-in-Docker (DinD) mode (doesn't work)
-::docker network create dind-network
-:: Fails with "ERROR: failed to connect to the docker API at tcp://docker:2375: lookup docker on 192.168.65.7:53: no such host"
-::set DOCKER_PARAMS=--privileged --network dind-network -e DOCKER_TLS_CERTDIR=/certs -v daemon-certs-ca:/certs/ca -v daemon-certs-client:/certs/client
-:: -e DOCKER_HOST=tcp://dind:2375 -v /certs/client -p 2375:2375
-::docker run %DOCKER_PARAMS% --rm -it gehc-dind sh
-::docker run %DOCKER_PARAMS% --rm -it gehc-dind sh -c "echo Trying to run docker..; docker build -f Ubuntu-24.04.Dockerfile ."
 
 echo Starting DinD container...
 docker run -d --privileged --name dind-instance gehc-dind
