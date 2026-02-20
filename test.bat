73ce463ce767b381e20e3f0736038dc3b37904fd
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-02-20 14:56:26 +0100

    .

--- test.bat
+++ test.bat
@@ -11,12 +11,12 @@ docker build -t gehc-dind -f dind.Dockerfile .
 
 echo Testing Docker-in-Docker...
 
-:: System-wide Docker-out-of-Docker socket binding mode (works nicely)
+:: System-wide Docker-out-of-Docker (DooD) socket binding mode (works nicely)
 set DIND_PARAMS=-v /var/run/docker.sock:/var/run/docker.sock
 
-:: Docker-in-Docker (dind) mode (doesn't work)
+:: Docker-in-Docker (DinD) mode (doesn't work)
 :: Fails with "ERROR: failed to connect to the docker API at tcp://docker:2375: lookup docker on 192.168.65.7:53: no such host"
-::set DIND_PARAMS=--privileged -e DOCKER_TLS_CERTDIR=/certs -v daemon-certs-ca:/certs/ca -v daemon-certs-client:/certs/client
+::set DIND_PARAMS=--privileged -e DOCKER_TLS_CERTDIR=/certs -v daemon-certs-ca:/certs/ca -v daemon-certs-client:/certs/client:ro
 :: -e DOCKER_HOST=tcp://dind:2375 -v /certs/client -p 2375:2375
 
 ::docker run %DIND_PARAMS% --rm -it gehc-dind sh
