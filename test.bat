3b193ea4d7541187ce7bad884b1980cfba6fb1d5
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-02-20 14:50:37 +0100

    wip

--- test.bat
+++ test.bat
@@ -16,7 +16,7 @@ set DIND_PARAMS=-v /var/run/docker.sock:/var/run/docker.sock
 
 :: Docker-in-Docker (dind) mode (doesn't work)
 :: Fails with "ERROR: failed to connect to the docker API at tcp://docker:2375: lookup docker on 192.168.65.7:53: no such host"
-::set DIND_PARAMS=--privileged -e DOCKER_TLS_CERTDIR=/certs -v /certs/client
+::set DIND_PARAMS=--privileged -e DOCKER_TLS_CERTDIR=/certs -v daemon-certs-ca:/certs/ca -v daemon-certs-client:/certs/client
 :: -e DOCKER_HOST=tcp://dind:2375 -v /certs/client -p 2375:2375
 
 ::docker run %DIND_PARAMS% --rm -it gehc-dind sh
