47bef1840ea18b47ed339d5d911c953cf270f854
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-02-20 14:31:34 +0100

    .

--- test.bat
+++ test.bat
@@ -13,10 +13,8 @@ echo Testing Docker-in-Docker...
 
 :: Socket binding mode
 set DIND_PARAMS=-v /var/run/docker.sock:/var/run/docker.sock
-
 :: Docker-in-Docker (dind) mode
 ::set DIND_PARAMS=--privileged -e DOCKER_TLS_CERTDIR=/certs -v /certs/client
 
 ::docker run %DIND_PARAMS% --rm -it gehc-dind sh
-
 docker run %DIND_PARAMS% --rm -it gehc-dind sh -c "echo Trying to run docker..; docker build -f Ubuntu-24.04.Dockerfile ."
