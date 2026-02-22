efc3b05686d72c9df1a958224f15c7231471c1c3
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-02-22 19:33:39 +0100

    reduce sleep to 1sec

--- DinD_test.bat
+++ DinD_test.bat
@@ -12,7 +12,7 @@ docker run -d --privileged --name dind-instance gehc-dind
 echo Connecting to DinD container...
 ::docker exec -it dind-instance sh
 :: Sleeping 2sec to avoid: ERROR: failed to connect to the docker API at tcp://docker:2375: lookup docker on 192.168.65.7:53: no such host
-docker exec -it dind-instance sh -c "sleep 2s; echo Trying to run docker..; docker build -f Ubuntu-24.04.Dockerfile ."
+docker exec -it dind-instance sh -c "sleep 1s; echo Trying to run docker..; docker build -f Ubuntu-24.04.Dockerfile ."
 
 echo Stopping DinD container...
 docker rm --force dind-instance
