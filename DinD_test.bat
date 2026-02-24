291329d270120315ddf79771a245b1436a1db16d
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-02-24 22:24:23 +0100

    use "docker compose -d --wait" to get rid of ad-hoc waiting for healthcheck to pass

--- DinD_test.bat
+++ DinD_test.bat
@@ -7,12 +7,7 @@ docker build -t gehc-dind -f dind.Dockerfile .
 echo Testing Docker-in-Docker...
 
 echo Starting DinD container...
-docker compose -f dind.compose.yaml up -d
-
-echo Waiting for container to start...
-:: TODO: Figure out how to get rid of this sleep step.
-:: Sleeping until container is fully started to avoid: ERROR: failed to connect to the docker API at tcp://docker:2375: lookup docker on 192.168.65.7:53: no such host
-docker exec -it dind-instance sh -c "until docker info > /dev/null; do sleep 1s; done"
+docker compose -f dind.compose.yaml up -d --wait
 
 echo Running DinD commands...
 ::docker exec -it dind-instance sh
