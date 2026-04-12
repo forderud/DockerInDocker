8f96bb31582cb82a5cb0903943fb407dfd8d766d
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-04-12 13:58:40 +0200

    .

--- run_shell.bat
+++ run_shell.bat
@@ -3,5 +3,6 @@
 docker run --rm -it docker:dind sh
 ::docker run --rm -it docker:cli sh
 
-:: Command to check if docker daemon is running:
+:: Commands to check if docker daemon is running (doesn't seem to work):
 :: docker info
+:: ps aux
