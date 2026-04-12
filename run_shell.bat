26607dd4ed7b91855d807d331e0183deb8ba2d5f
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-04-12 14:02:05 +0200

    .

--- run_shell.bat
+++ run_shell.bat
@@ -1,6 +1,6 @@
 :: Open an interactive bash shell inside the container
 
-docker run --rm -it docker:dind sh
+docker run --privileged --rm -it docker:dind sh
 ::docker run --rm -it docker:cli sh
 
 :: Commands to check if docker daemon is running (doesn't seem to work):
