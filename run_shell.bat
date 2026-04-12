1469862fa23493d6c57417b6d501d03f0a298af8
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-04-12 14:08:42 +0200

    Fix DooD command

--- run_shell.bat
+++ run_shell.bat
@@ -1,7 +1,7 @@
 :: Open an interactive bash shell inside the container
 
 docker run --privileged --rm -it docker:dind sh
-::docker run --rm -it docker:cli sh
+::docker run -v /var/run/docker.sock:/var/run/docker.sock --rm -it docker:cli sh
 
 :: Commands to check if docker daemon is running (doesn't seem to work):
 :: docker info
