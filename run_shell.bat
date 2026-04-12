fdb83f404f96bdb1ec31fc5f890e3ae5ea88ec30
Fredrik Orderud <forderud@gmail.com>
2026-04-12 15:19:47 +0200

    Working DinD baseline

--- run_shell.bat
+++ run_shell.bat
@@ -1,11 +1,13 @@
 :: Open an interactive bash shell inside the container
 
-:: Docker-in-Docker (DinD) - doesn't work
+:: Docker-in-Docker (DinD):
 :: --network=host
 :: -e DOCKER_TLS_CERTDIR=""
 :: -e DOCKER_TLS_CERTDIR=/certs
 :: -e DOCKER_HOST=tcp://localhost:2375
-docker run --privileged --rm -it docker:dind sh
+docker run -d --privileged --name dind-instance docker:dind
+docker exec -it dind-instance sh -c "docker info; ps aux"
+docker rm --force dind-instance
 
 :: Docker-out-of-Docker (DooD):
 ::docker run -v /var/run/docker.sock:/var/run/docker.sock --rm -it docker:cli sh
